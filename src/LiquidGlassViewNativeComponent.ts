e1b130ec338803d7a025164bc0a815d971d4cce7
Pritam Nanda <pritamnanda504@gmail.com>
2026-08-02 02:22:06 +0530

    Fixed import types

--- src/LiquidGlassViewNativeComponent.ts
+++ src/LiquidGlassViewNativeComponent.ts
@@ -2,17 +2,17 @@
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 import type { HostComponent, ViewProps } from 'react-native';
 // @ts-ignore
-import type { WithDefault, Float, Int32 } from 'react-native/Libraries/Types/CodegenTypes';
+import type { WithDefault, Float } from 'react-native/Libraries/Types/CodegenTypes';
 
 interface NativeProps extends ViewProps {
-  refractionAmount?: Float;
-  blurAmount?: number;
-  edgeWidth?: number;
-  curvatureBlend?: number;
-  dispersion?: number;
-  saturationBoost?: number;
-  contrastBoost?: number;
-  brightnessLift?: number;
+  refractionAmount?: WithDefault<Float, 40>;
+  blurAmount?: WithDefault<Float, 30>;
+  edgeWidth?: WithDefault<Float, 24>;
+  curvatureBlend?: WithDefault<Float, 0.5>;
+  dispersion?: WithDefault<Float, 0.35>;
+  saturationBoost?: WithDefault<Float, 1.1>;
+  contrastBoost?: WithDefault<Float, 1.05>;
+  brightnessLift?: WithDefault<Float, 0.02>;
   tintColor?: string;
 }
 
